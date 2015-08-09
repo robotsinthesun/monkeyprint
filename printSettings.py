44d57ab357b8ad29c8ae03948dcae81f54349de8
paul <robotsinthesun@noreply.github.com>
2015-08-09 15:39:13 +0200

    Added license info

--- printSettings.py
+++ printSettings.py
@@ -1,5 +1,23 @@
-#!/usr/bin/python
 # -*- coding: latin-1 -*-
+
+#	Copyright (c) 2015 Paul Bomke
+#	Distributed under the GNU GPL v2.
+#
+#	This file is part of monkeyprint.
+#
+#	monkeyprint is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	monkeyprint is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You have received a copy of the GNU General Public License
+#    along with monkeyprint.  If not, see <http://www.gnu.org/licenses/>.
+
 
 class printSettings:
 	def __init__(self):
