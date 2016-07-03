8ab6fe45589c10d16d88b37c71f71111abfec6cd
robotsinthesun <paul@robotsinthesun.org>
2016-07-03 20:33:23 +0000

    Added install script for Raspberry Pi

--- raspberryPiInstall.sh
+++ raspberryPiInstall.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
-sudo cp ./raspberryPiStartup.sh /etc/init.d/
-sudo chmod 755 /etc/init.d/raspberryPiStartup.sh
-sudo update-rc.d raspberryPiStartup.sh defaults
+sudo sed '/exit/ i sudo startx /usr/bin/python /home/pi/monkeyprint/monkeyprint.py --server' /etc/rc.local &
+#sudo cp ./raspberryPiStartup.sh /etc/init.d/
+#sudo chmod 755 /etc/init.d/raspberryPiStartup.sh
+#sudo update-rc.d raspberryPiStartup.sh defaults
