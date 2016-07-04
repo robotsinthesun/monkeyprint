8e1bb9a70adc40bd7a55e8ef7895557b6448a386
robotsinthesun <paul@robotsinthesun.org>
2016-07-04 14:46:09 +0200

    Small fix to raspberry pi install script.

--- raspberryPiInstall.sh
+++ raspberryPiInstall.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-sudo sed '/exit/ i sudo startx /usr/bin/python /home/pi/monkeyprint/monkeyprint.py --server' /etc/rc.local &
+sudo sed -i '/exit/ i sudo startx /usr/bin/python /home/pi/monkeyprint/monkeyprint.py --server' /etc/rc.local &
 #sudo cp ./raspberryPiStartup.sh /etc/init.d/
 #sudo chmod 755 /etc/init.d/raspberryPiStartup.sh
 #sudo update-rc.d raspberryPiStartup.sh defaults
