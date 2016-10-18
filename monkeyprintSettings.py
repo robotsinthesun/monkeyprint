e2110ea0bec036d4560ada502029454db71f2f85
robotsinthesun <paul@robotsinthesun.org>
2016-10-18 19:41:23 +0200

    Set lower bound for model scaling to avoid div by 0 error.

--- monkeyprintSettings.py
+++ monkeyprintSettings.py
@@ -74,7 +74,7 @@ class modelSettings(dict):
 		# Create objects for all the settings and put them into dictionary.
 		self['filename'] = setting(value="")
 		self['active'] = setting(value=True)
-		self['scaling'] = setting(value=1, name='Scaling')
+		self['scaling'] = setting(value=1, name='Scaling', lower=0.0000000000001)
 		self['rotationX'] = setting(value=0,	lower=0,	upper=359,	unit='°',		name='Rotation X')
 		self['rotationY'] = setting(value=0,	lower=0,	upper=359,	unit='°',		name='Rotation Y')
 		self['rotationZ'] = setting(value=0,	lower=0,	upper=359,	unit='°',		name='Rotation Z')
